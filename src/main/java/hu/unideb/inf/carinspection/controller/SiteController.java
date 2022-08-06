293183a272283a1b131f4b262a62f4b9b77c25ec
Halász Dániel <halaszdd@gmail.com>
2022-08-06 17:13:59 +0200

    implemented /api/site/all/ endpoint

---
+++ src/main/java/hu/unideb/inf/carinspection/controller/SiteController.java
@@ -0,0 +1,23 @@
+package hu.unideb.inf.carinspection.controller;
+
+import hu.unideb.inf.carinspection.data.SiteRepository;
+import hu.unideb.inf.carinspection.domain.Site;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SiteController {
+
+    private final SiteRepository siteRepository;
+
+    public SiteController(SiteRepository siteRepository) {
+        this.siteRepository = siteRepository;
+    }
+
+    @GetMapping("/api/site/all/")
+    public List<Site> getAllSites() {
+        return siteRepository.findAll();
+    }
+}
