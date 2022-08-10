053a1292709009f4e88d1221c1f7b6983177caa0
Halász Dániel <halaszdd@gmail.com>
2022-08-10 18:04:40 +0200

    Fixed login

--- src/main/java/hu/unideb/inf/carinspection/CarInspectionApplication.java
+++ src/main/java/hu/unideb/inf/carinspection/CarInspectionApplication.java
@@ -2,6 +2,7 @@ package hu.unideb.inf.carinspection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class CarInspectionApplication {
--- src/main/java/hu/unideb/inf/carinspection/WebSecurityConfig.java
+++ src/main/java/hu/unideb/inf/carinspection/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,6 +16,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
 
     public WebSecurityConfig(@Qualifier("defaultUserDetailsService") UserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
@@ -34,8 +37,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll().and().csrf().disable().cors().disable();
     }
 
-    private final UserDetailsService userDetailsService;
-
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
@@ -45,5 +46,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
+        auth.authenticationProvider(authenticationProvider());
     }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(getPasswordEncoder());
+
+        return authProvider;
+    }
+
 }
