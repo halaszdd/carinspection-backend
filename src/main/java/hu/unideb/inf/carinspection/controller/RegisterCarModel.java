dc976b87cba6b1240872506122b01faa392c1566
Halász Dániel <halaszdd@gmail.com>
2022-08-16 18:14:56 +0200

    added validation to RegisterCarModel

--- src/main/java/hu/unideb/inf/carinspection/controller/RegisterCarModel.java
+++ src/main/java/hu/unideb/inf/carinspection/controller/RegisterCarModel.java
@@ -3,8 +3,10 @@ package hu.unideb.inf.carinspection.controller;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 
@@ -15,6 +17,7 @@ public class RegisterCarModel {
     @NotNull
     private String plateNumber;
     @NotNull
+    @Size(min = 17, max = 17)
     private String vin;
     @NotNull
     private LocalDate expirationDate;
